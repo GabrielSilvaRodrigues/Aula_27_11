d2318b625f4e7db60979a93af8cfaa373a8869cf
Gabriel da Silva <gabriel.rodrigues106@fatec.sp.gov.br>
2024-11-27 11:25:55 -0300

    Update Pokedex.java

--- App/src/Pokedex.java
+++ App/src/Pokedex.java
@@ -2,11 +2,39 @@ public class Pokedex {
     public static void main(String[] args) throws Exception {
         Eevee e1 = new Eevee();
         Jolteon j1 = new Jolteon();
+        Vaporeon v1= new Vaporeon();
+        Flareon f1 = new Flareon();
+        Espeon es1 = new Espeon();
+        Umbreon u1 = new Umbreon();
+        Leafeon l1 = new Leafeon();
+        Glaceon g1 = new Glaceon();
+        Sylveon s1 = new Sylveon();
         System.out.println("Ataque: "+e1.ataque());
         System.out.println("Defesa: "+e1.defesa());
         System.out.println("Especial: "+e1.especial());
         System.out.println("Ataque: "+j1.ataque());
         System.out.println("Defesa: "+j1.defesa());
         System.out.println("Especial: "+j1.especial());
+        System.out.println("Ataque: "+v1.ataque());
+        System.out.println("Defesa: "+v1.defesa());
+        System.out.println("Especial: "+v1.especial());
+        System.out.println("Ataque: "+f1.ataque());
+        System.out.println("Defesa: "+f1.defesa());
+        System.out.println("Especial: "+f1.especial());
+        System.out.println("Ataque: "+es1.ataque());
+        System.out.println("Defesa: "+es1.defesa());
+        System.out.println("Especial: "+es1.especial());
+        System.out.println("Ataque: "+u1.ataque());
+        System.out.println("Defesa: "+u1.defesa());
+        System.out.println("Especial: "+u1.especial());
+        System.out.println("Ataque: "+l1.ataque());
+        System.out.println("Defesa: "+l1.defesa());
+        System.out.println("Especial: "+l1.especial());
+        System.out.println("Ataque: "+g1.ataque());
+        System.out.println("Defesa: "+g1.defesa());
+        System.out.println("Especial: "+g1.especial());
+        System.out.println("Ataque: "+s1.ataque());
+        System.out.println("Defesa: "+s1.defesa());
+        System.out.println("Especial: "+s1.especial());
     }
 }
