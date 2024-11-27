60c2d20205880bc4125de284d29e8be307d30f7b
Gabriel da Silva <gabriel.rodrigues106@fatec.sp.gov.br>
2024-11-27 13:39:13 +0000

    mudancas

--- App/src/Jolteon.java
+++ App/src/Jolteon.java
@@ -1,10 +1,13 @@
 public class Jolteon extends Eevee{
+    @Override
     public String ataque(){
         return "Trovoada - Thunder Shock";
     }
+    @Override
     public String especial(){
         return "Raio - Thunderbold";
     }
+    @Override
     public String defesa(){
         return "Carga - Wild Charge";
     }
